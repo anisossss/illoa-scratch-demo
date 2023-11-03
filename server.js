0a9f988b9a527e0294a54efb1ddddd810e09ddd9
anis <anis.khalef98@gmail.com>
2023-11-03 02:05:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/pattern1", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/pattern1/index.html"));
+});
+app.get("/pattern2", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/pattern2/index.html"));
+});
+app.get("/circle", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/circle/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
