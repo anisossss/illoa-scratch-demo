a8cf66db05fb8f62e7847952d444d19abbb8b050
anis <anis.khalef98@gmail.com>
2023-11-16 00:48:02 +0100

    latest_commit

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/illoa", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/illoa/index.html"));
 });
-
+app.get("/goal", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/goal/index.html"));
+});
+app.get("/khazna", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/khazna/index.html"));
+});
+app.get("/septvivante", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "/septvivante/index.html"));
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
