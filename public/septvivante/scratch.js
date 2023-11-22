873f386ac67f4ef8051c7d42422fb19c794afe11
anis <anis.khalef98@gmail.com>
2023-11-22 14:14:50 +0100

    first commit

--- public/septvivante/scratch.js
+++ public/septvivante/scratch.js
@@ -6,8 +6,8 @@ const backgroundImage = new Image();
 const topImage = new Image();
 const brushImage = new Image();
 
-backgroundImage.src = "../assets/images/BG7VIVANTE.png";
-topImage.src = "../assets/images/7VIVANTE.png";
+backgroundImage.src = "../assets/images/BG7VIVANTE.svg";
+topImage.src = "../assets/images/7VIVANTE.svg";
 brushImage.src = "../assets/brushs/brush1.png";
 let isDrawing = false;
 let offscreenCanvas, offscreenCtx;
